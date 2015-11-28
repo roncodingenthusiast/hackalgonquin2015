4f59604273e4b89acc2d9b5f9a4048c460cfbf51
roncodingenthusiast <ronekambi@gmail.com>
2015-11-28 13:08:50 -0500

    Added multiple markers for the g maps

--- Geolocation/js/controller/app.js
+++ Geolocation/js/controller/app.js
@@ -7,6 +7,7 @@ myApp.directive("myMaps", function(){
 		replace: true,
 		link: function(scope, element, attrs){
 			var myLatLng = new google.maps.LatLng(45.2843691, -75.732333);
+			var myLatLng2 = new google.maps.LatLng(56.2843691, -70.732333);
 			var mapOptions = {
 				center: myLatLng,
 				zoom: 5,
@@ -18,7 +19,13 @@ myApp.directive("myMaps", function(){
 				map: map, 
 				title: "hello world!"
 			});
+			var marker2 = new google.maps.Marker({
+				position: myLatLng2, 
+				map: map, 
+				title: "hello"
+			});
 			marker.setMap(map);
+			marker2.setMap(map);
 		}
 	};
 } );
