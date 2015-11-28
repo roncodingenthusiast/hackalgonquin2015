428e9d9b0d8405c770de016c7bfd063ad435b61d
roncodingenthusiast <ronekambi@gmail.com>
2015-11-28 10:28:44 -0500

    google map added
    google map with marker showing

--- Geolocation/js/controller/app.js
+++ Geolocation/js/controller/app.js
@@ -1,1 +1,24 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var myApp = angular.module('myApp', []);
+
+myApp.directive("myMaps", function(){
+	return{
+		restrict:'E',
+		template:'<div></div>',
+		replace: true,
+		link: function(scope, element, attrs){
+			var myLatLng = new google.maps.LatLng(45.2843691, -75.732333);
+			var mapOptions = {
+				center: myLatLng,
+				zoom: 5,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
+			var marker = new google.maps.Marker({
+				position: myLatLng, 
+				map: map, 
+				title: "hello world!"
+			});
+			marker.setMap(map);
+		}
+	};
+} );
--- Geolocation/js/controller/controllers.js
+++ Geolocation/js/controller/controllers.js
@@ -1,13 +1,16 @@
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+myApp.controller('PhoneListCtrl', function ($scope) {
   	if(navigator.geolocation){
     	navigator.geolocation.getCurrentPosition(function(position){
     			$scope.$apply(function(){
     			$scope.position = position;
         });
         console.log(position);
+        $scope.latitude = position.coords.latitude;
+        $scope.longititude = position.coords.longititude;
         console.log(position.coords.latitude);
         console.log(position.coords.longititude);
   		});
 
   	}
+    
 });
