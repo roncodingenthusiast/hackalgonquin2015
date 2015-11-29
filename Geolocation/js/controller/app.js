e6388604ee42e9aa4421f9d822ca396c330cbba2
roncodingenthusiast <ronekambi@gmail.com>
2015-11-29 16:31:48 -0500

    added array with 3 element and click opens the info

--- Geolocation/js/controller/app.js
+++ Geolocation/js/controller/app.js
@@ -7,7 +7,7 @@ myApp.directive("myMaps", [ function(){
 		link: function(scope, element, attrs){
 			
 			var myLatLng = new google.maps.LatLng(45.2843691, -75.732333);
-			var myMarkers = [[45, -75], [56, -70], [58, -69]];
+			var myMarkers = [["park number 1", 45, -75, "location"], ["park number 2", 56, -70, "location2"], ["park number 3", 58, -69, "location3"]];
 			var mapOptions = {
 				center: myLatLng,
 				zoom: 5,
@@ -19,13 +19,13 @@ myApp.directive("myMaps", [ function(){
 			
 			for(var i =0; i < myMarkers.length; i++){
 				marker = new google.maps.Marker({
-					position: new google.maps.LatLng(myMarkers[i][0], myMarkers[i][1]), 
+					position: new google.maps.LatLng(myMarkers[i][1], myMarkers[i][2]), 
 					map: map, 
 					title: "hellow world!"+i
 				});
 				google.maps.event.addListener(marker, 'click', (function(marker, i){
 					return function(){
-						infoWindow.setContent("try");
+						infoWindow.setContent(myMarkers[i][0] + ' | located at | '+myMarkers[i][3]);
 						infoWindow.open(map, marker);
 					}
 				})(marker, i));
